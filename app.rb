7fcb90e1a8e720b0746f24dd96dfad1c18cd158d
Joshua Delsman <jdelsman@voxxit.com>
2014-09-18 14:19:36 -0700

    Adding root redirect, and try to fix forward slash bug

--- app.rb
+++ app.rb
@@ -2,63 +2,52 @@ require 'guillotine'
 require 'redis'
 
 module Katana
-    class App < Guillotine::App
-      # use redis adapter with redistogo
-      uri = URI.parse(ENV["REDISTOGO_URL"])
-      REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-      adapter = Guillotine::Adapters::RedisAdapter.new REDIS
-      set :service => Guillotine::Service.new(adapter, :strip_query => false,
-                                              :strip_anchor => false)
+  class App < Guillotine::App
 
-      # authenticate everything except GETs
-      before do
-        unless request.request_method == "GET"
-          protected!
+    uri   = URI.parse ENV["REDISTOGO_URL"]
+
+    redis = Redis.new host: uri.host,
+                      port: uri.port,
+                      password: uri.password
+
+    adapter = Guillotine::Adapters::RedisAdapter.new redis
+
+    set service: Guillotine::Service.new adapter
+
+    # authenticate everything except GETs
+    before { protected! if request.request_method != "GET" }
+
+    get "/" do
+      redirect "http://voxxit.com"
+    end
+
+    helpers do
+
+      # Private: helper method to protect URLs with Rack Basic Auth
+      #
+      # Throws 401 if authorization fails
+      def protected!
+        return unless ENV["HTTP_USER"]
+
+        unless authorized?
+          response['WWW-Authenticate'] = %(Basic realm="Restricted Area")
+          throw(:halt, [401, "Not authorized\n"])
         end
       end
 
-      get '/' do
-        "Shorten all the URLs"
-      end
+      # Private: helper method to check if authorization parameters match the set environment variables
+      #
+      # Returns true or false
+      def authorized?
+        @auth ||= Rack::Auth::Basic::Request.new(request.env)
 
-      if ENV['TWEETBOT_API']
-        # experimental (unauthenticated) API endpoint for tweetbot
-        get '/api/create/?' do
-          status, head, body = settings.service.create(params[:url], params[:code])
-
-          if loc = head['Location']
-            "#{File.join("http://", request.host, loc)}"
-          else
-            500
-          end
-        end
-      end
-
-      # helper methods
-      helpers do
-
-        # Private: helper method to protect URLs with Rack Basic Auth
-        #
-        # Throws 401 if authorization fails
-        def protected!
-          return unless ENV["HTTP_USER"]
-          unless authorized?
-            response['WWW-Authenticate'] = %(Basic realm="Restricted Area")
-            throw(:halt, [401, "Not authorized\n"])
-          end
-        end
-
-        # Private: helper method to check if authorization parameters match the
-        # set environment variables
-        #
-        # Returns true or false
-        def authorized?
-          @auth ||=  Rack::Auth::Basic::Request.new(request.env)
-          user = ENV["HTTP_USER"]
-          pass = ENV["HTTP_PASS"]
-          @auth.provided? && @auth.basic? && @auth.credentials && @auth.credentials == [user, pass]
-        end
+        @auth.provided? && @auth.basic? && @auth.credentials && @auth.credentials == [
+          ENV["HTTP_USER"],
+          ENV["HTTP_PASS"]
+        ]
       end
 
     end
+
+  end
 end
